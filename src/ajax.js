49b9593c9c368ca122f31bbaba6b23ee682bf186
goohooh <goo_oo@naver.com>
2016-07-27 02:17:26 +0900

    update ajax method: refine response data

--- src/ajax.js
+++ src/ajax.js
@@ -1,10 +1,10 @@
 // XML은 지원하지 않음
 // ajax_params = {
-//	method: 'GET, POST, PUT, ...',
-//	url: URL for API,
-//	success: function(data) { ... },
-//	error: function(error) { ... }, [optional]
-//	complete: function(data) { ... } [optional]
+// 	method: 'GET, POST, PUT, ...',
+// 	url: URL for API,
+// 	success: function(data) { ... },
+// 	error: function(error) { ... }, [optional]
+// 	complete: function(data) { ... } [optional]
 // }
 $.ajax = function(ajax_params){
 	var request = new XMLHttpRequest(),
@@ -29,19 +29,28 @@ $.ajax = function(ajax_params){
 	request.onreadystatechange = function( event ) {
 		if (request.readyState == 4) {
 			if (200 <= request.status && request.status < 300) {
-				success(request.responseText);
+				success({
+					status: request.status,
+					statusText: request.statusText,
+					responseData: request.responseText
+				});
 			} else {
 				if (ajax_params.error) {
-					ajax_params.error(request);
+					ajax_params.error({
+						status: request.status,
+						statusText: request.statusText,
+						responseData: request.responseText
+					});
 				}
 			}
 
 			if (ajax_params.complete) {
-				ajax_params.complete(request);
+				ajax_params.complete({
+					status: request.status,
+					statusText: request.statusText,
+					responseData: request.responseText
+				)};
 			}
-			// console.log(request.responseText);
 		}
 	}
-
-
 }
